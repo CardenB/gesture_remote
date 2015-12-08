dcfb404aa73958aa497238b8824cc977185ca7c4
Carden Bagwell <cardenbag@gmail.com>
2015-12-07 22:07:47 -0500

    Final version of the code in remote.cpp

--- tue_code/remote.cpp
+++ tue_code/remote.cpp
@@ -1,10 +1,10 @@
-
 #define DIST_MAX 40 // Maximum distance in cm, anything above is ignored.
-#define DIST_THRES                                                             \
-  20 // Threshold for up/down commands. If higher, command is "up". If lower,
-     // "down".
+// Threshold for up/down commands.
+// If higher, command is "up". If lower, "down".
+#define DIST_THRES 20
 
 #include "mbed.h"
+#include "rtos.h"
 #include "hcsr04.h"
 #include "SI1143.h"
 
@@ -20,10 +20,12 @@ DigitalOut l4(LED4);
 int value;
 
 unsigned int dist;
+int sense1, sense2, sense3;
 
 const float period = 1.0;
 const float power_execute_count = 3.0 / period;
 float power_count = 0;
+Mutex led_mut;
 
 bool flag = false;
 
@@ -37,10 +39,23 @@ enum MODE {
 };
 MODE mode = NONE;
 
+void set_led(uint8_t led_vals) {
+  led_mut.lock();
+  l1 = led_vals & 1;
+  led_vals = led_vals >> 1;
+  l2 = led_vals & 1;
+  led_vals = led_vals >> 1;
+  l3 = led_vals & 1;
+  led_vals = led_vals >> 1;
+  l4 = led_vals & 1;
+  led_mut.unlock();
+}
+
 void ExecutePowerCommand() {
+  led_mut.lock();
   pc.printf("Power!");
-  l4 = 1;
-  l3 = 1;
+  led_mut.unlock();
+  set_led(15);
 }
 
 void CheckForPower() {
@@ -51,14 +66,14 @@ void CheckForPower() {
     ++power_count;
   } else {
     power_count = 0;
-    l4 = 0;
-    l3 = 0;
+    set_led(0);
   }
 }
 
 Timer t;
 
 int timer;
+
 
 int handleDist(int dist) {
   if (dist > DIST_THRES) {
@@ -69,81 +84,73 @@ int handleDist(int dist) {
 }
 
 void parallax_thread(void const *args) {
-  sense1 = sensor.get_ps1(1);
-  sense2 = sensor.get_ps2(1);
-  sense3 = sensor.get_ps3(1);
+  while (true) {
+    sense1 = sensor.get_ps1(1);
+    sense2 = sensor.get_ps2(1);
+    sense3 = sensor.get_ps3(1);
 
-  if (sense1 > 80 || sense2 > 80 || sense3 > 80) {
-    if (sense1 > sense2 && sense1 > sense3) {
-      l1 = 1;
-      l2 = 0;
-      l3 = 0;
-      mode = CHANNEL;
-    }
+    if (sense1 > 80 || sense2 > 80 || sense3 > 80) {
+      if (sense1 > sense2 && sense1 > sense3) {
+        set_led(1);
+        mode = CHANNEL;
+      }
 
-    else if (sense2 > sense1 && sense2 > sense3) {
-      mode = POWER;
-      l1 = 1;
-      l2 = 1;
-      l3 = 0;
-    }
+      else if (sense2 > sense1 && sense2 > sense3) {
+        mode = POWER;
+        set_led(3);
+      }
 
-    else if (sense3 > sense1 && sense3 > sense2) {
-      l1 = 0;
-      l2 = 1;
-      l3 = 0;
-      mode = VOLUME;
+      else if (sense3 > sense1 && sense3 > sense2) {
+        set_led(2);
+        mode = VOLUME;
+      }
     }
+    Thread::wait(50);
   }
 }
 
 void ultrasound_thread(void const *args) {
-  usensor.start();
-  dist = usensor.get_dist_cm();
-  if (dist < DIST_MAX) {
-    int d = 500; // first delay is longer for single volume change
+  while (true) {
+    usensor.start();
+    dist = usensor.get_dist_cm();
+    if (dist < DIST_MAX) {
+      int d = 500; // first delay is longer for single volume change
 
-    while (dist < DIST_MAX) {
-      value = handleDist(dist); // is up or down?
-      // doIR(CONTROL_VOL, value); // fire off IR signal
-      if (value == 1) {
-        l4 = 1;
-        wait_ms(100);
-        l4 = 0;
-      } else {
-        l3 = 1;
-        wait_ms(100);
-        l3 = 0;
+      while (dist < DIST_MAX) {
+        value = handleDist(dist); // is up or down?
+        // doIR(CONTROL_VOL, value); // fire off IR signal
+        if (value == 1) {
+          set_led(8);
+          Thread::wait(100);
+          set_led(0);
+        } else {
+          set_led(4);
+          Thread::wait(100);
+          set_led(0);
+        }
+        usensor.start();
+        Thread::wait(d);              // wait
+        dist = usensor.get_dist_cm(); // check for hand again
+        d = 100; // delays are shorter for quick multiple volume adjustment
       }
-      usensor.start();
-      wait_ms(d);                   // wait
-      dist = usensor.get_dist_cm(); // check for hand again
-      d = 100; // delays are shorter for quick multiple volume adjustment
+      // this stops accidental channel change after volume adjustment
+      Thread::wait(500);
     }
-    wait_ms(
-        500); // this stops accidental channel change after volume adjustment
+    Thread::wait(50); // Short enough to detect all swipes.
   }
-  wait_ms(50); // Short enough to detect all swipes.
 }
 
 int main() {
 
   power_ticker.attach(&CheckForPower, period);
-  l1 = 0;
-  l2 = 0;
-  l3 = 0;
-  l4 = 0;
-  int sense1, sense2, sense3;
-
+  set_led(0);
   // Setup the baseline
   sensor.bias(1, 5);
   wait(1);
   Thread mode_checker(parallax_thread);
   Thread range_checker(ultrasound_thread);
 
-  /*
   while (1) {
-    // pc.printf("%ld \n\r", dist); //debug
+    // spin.
   }
-  */
 }
